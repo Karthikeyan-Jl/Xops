bc74b7b17481735d6504cce5c22d80b3af9b9841
Karthikeyan-Jl <jkarthik@celcomsolutions.com>
2025-08-25 11:26:46 +0530

    updated

--- Task3/app.py
+++ Task3/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Hello from Flask via CI/CD on Amazon Linux EC2!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
+    # Expose to all interfaces on port 5000
     app.run(host="0.0.0.0", port=5000)
